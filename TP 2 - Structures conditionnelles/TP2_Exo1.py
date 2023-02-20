4c00a196121aa8130d0d04175556a33cbdc7b5e2
StrixRange <94807354+StrixRange@users.noreply.github.com>
2023-02-20 12:48:28 +0100

    Add files via upload

---
+++ TP 2 - Structures conditionnelles/TP2_Exo1.py
@@ -0,0 +1,9 @@
+# Exercice 1
+
+a = int(input("Entrez un entier a : "))
+b = int(input("Entrez un entier b : "))
+
+if a < b:
+    print(a)
+else:
+    print(b)
---
+++ TP 2 - Structures conditionnelles/TP2_Exo7.py
@@ -0,0 +1,5 @@
+# Exercice 7
+# iL INTERROGE PAS SUR LES 2 DERNIERS EXO + franceioi
+
+masque = int(input("Quantité de masques désirée : "))
+
